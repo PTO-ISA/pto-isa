404d0f36ed4027c0ed7fa313cdc9fba397dd6ec0
ivanmang <mang.hao.xiang@huawei.com>
2026-03-13 11:46:16 +0800

    [BugFix]TSEL && TSELS update
    Co-authored-by: ivanmang<mang.hao.xiang@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !460 merge master into master
    
    [BugFix]TSEL && TSELS update
    
    Created-by: qq_53648788
    Commit-by: ivanmang
    Merged-by: cann-robot
    Description: ## 描述
    原本的TSEL && TSELS直接在kernel中硬编码tmpbuffer的地址，需要修改
    TSEL && TSELS的接口新增tmpTile入参，同步修改ut、prelu实现
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/pto-isa!460

--- tests/npu/kirin9030/src/st/testcase/tsel/tsel_kernel.cpp
+++ tests/npu/kirin9030/src/st/testcase/tsel/tsel_kernel.cpp
@@ -36,10 +36,13 @@ __global__ AICORE void runTSel(__gm__ T *out, __gm__ uint8_t *mask, __gm__ T *sr
     using MaskGlobal = GlobalTensor<uint8_t, DynShapeDim5m, DynStridDim5m>;
 
     using MaskTile = Tile<TileType::Vec, uint8_t, maskRow, maskCol, BLayout::RowMajor, -1, -1>;
+    using TmpTile = Tile<TileType::Vec, uint8_t, 1, 32, BLayout::RowMajor, -1, -1>;
+
     TileData src0Tile(ValidRows, ValidCols);
     TileData src1Tile(ValidRows, ValidCols);
     TileData dstTile(ValidRows, ValidCols);
     MaskTile maskTile(maskVRow, maskVCol);
+    TmpTile tmpTile(1, 32);
     constexpr uint64_t tileSize = Rows * Cols * sizeof(T);
     constexpr uint64_t maskSize = maskVRow * maskVCol;
     constexpr uint64_t totalSize = tileSize * 3 + maskSize;
@@ -49,6 +52,7 @@ __global__ AICORE void runTSel(__gm__ T *out, __gm__ uint8_t *mask, __gm__ T *sr
     TASSIGN(src1Tile, (uint64_t)(tileSize));
     TASSIGN(dstTile, (uint64_t)(tileSize * 2));
     TASSIGN(maskTile, (uint64_t)(tileSize * 3));
+    TASSIGN(maskTile, (uint64_t)(tileSize * 4));
 
     GlobalData src0Global(src0);
     GlobalData src1Global(src1);
@@ -60,7 +64,7 @@ __global__ AICORE void runTSel(__gm__ T *out, __gm__ uint8_t *mask, __gm__ T *sr
     TLOAD(maskTile, maskGlobal);
     set_flag(PIPE_MTE2, PIPE_V, EVENT_ID0);
     wait_flag(PIPE_MTE2, PIPE_V, EVENT_ID0);
-    TSEL<TileData, MaskTile>(dstTile, maskTile, src0Tile, src1Tile);
+    TSEL(dstTile, maskTile, src0Tile, src1Tile, tmpTile);
     set_flag(PIPE_V, PIPE_MTE3, EVENT_ID0);
     wait_flag(PIPE_V, PIPE_MTE3, EVENT_ID0);
     TSTORE(dstGlobal, dstTile);
